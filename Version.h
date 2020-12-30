dee45ad0a304e2a1cbd74466309ec2d65e71ebac
destroyedlolo <l_faillie@yahoo.com>
2020-12-30 19:10:54 +0100

    Empty skeleton

---
+++ Version.h
@@ -0,0 +1,12 @@
+/************************************************
+*	this file export 5MinutesMore's version
+*	and trace history of the application
+*
+*	History :
+*	---------
+*	30/12/2020 - LF : First version
+*/
+
+#ifndef VERSION_H
+#define VERSION_H "0.0100"
+#endif
---
+++ config.h
@@ -0,0 +1,8 @@
+// => Hardware select
+#define LILYGO_LILYPI_V1
+
+// => Function select
+#define LILYGO_BLOCK_ST7796S_MODULE	//Use ST7796S screen
+#define LILYGO_WATCH_LVGL			//Use LVGL
+
+#include <LilyGoWatch.h>
