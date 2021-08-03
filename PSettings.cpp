9ca7d495b74f234c63533d038871289cab5471b6
destroyedlolo <l_faillie@yahoo.com>
2021-08-04 01:34:01 +0200

    Add container

--- PSettings.cpp
+++ PSettings.cpp
@@ -13,16 +13,20 @@ LV_IMG_DECLARE(image_piscine_32px);
 PSettings::PSettings( lv_obj_t *np ) : Page( np, true ){
 
 		/* Pool mode */
-	
-	this->poolIcon = new Image( this->getMyself() );
-	this->poolIcon->Align( LV_ALIGN_IN_TOP_LEFT );
-	this->poolIcon->Set( &image_piscine_32px );
-/*
-	this->syncIcon->setClickable( true );
-	this->syncIcon->attacheEventeHandler( syncTime );
-*/
+	this->poolCont = new Container( this->getMyself() );
+	this->poolCont->setBgOpacity(0);
+	this->poolCont->Align( LV_ALIGN_IN_TOP_LEFT );
+	this->poolCont->setFit( LV_FIT_TIGHT );	// Its size is the one of it's child
+	this->poolCont->AutoRealign();	// otherwise the icon is shifted
+	this->poolCont->setPadding(0);
+	this->poolCont->setClickable( false);
 
-	this->pool = new DropDown( this->getMyself() );
+	this->poolIcon = new Image( this->poolCont );
+	this->poolIcon->Set( &image_piscine_32px );
+	this->poolIcon->Align( LV_ALIGN_IN_LEFT_MID );
+	this->poolIcon->setClickable( false );
+
+	this->pool = new DropDown( this->poolCont );
 	this->pool->addStyle( dropdownStyle );
 	this->pool->setWidth( 160 );
 	this->pool->setChoices( "Heures Creuses\nArret\nForce", true );
--- PSettings.h
+++ PSettings.h
@@ -6,11 +6,13 @@
 #define PSETTINGS_H
 
 #include <Page.h>
+#include <Container.h>
 #include <DropDown.h>
 #include <Image.h>
 
 class PSettings : public Page {
 
+	Container	*poolCont;
 	Image		*poolIcon;
 	DropDown	*pool;
 
